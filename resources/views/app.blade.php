b96946aed2ce07da6b6c37e25d6e3d3a50244695
Nursandi <sandinur157@gmail.com>
2024-04-18 21:59:46 +0700

    feat: Setup Prettier and TailwindCSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,8 +8,11 @@
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,400..700;1,400..700&family=Inter:wght@400..700&display=swap"
+        rel="stylesheet">
 
     <!-- Scripts -->
     @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,45 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.tsx",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
+        primary: {
+          DEFAULT: colors.blue[600],
+          dark: colors.blue[700],
+        },
+        success: {
+          DEFAULT: colors.emerald[600],
+          dark: colors.emerald[700],
+        },
+        danger: {
+          DEFAULT: colors.red[600],
+          dark: colors.red[700],
+        },
+      },
     },
+  },
 
-    plugins: [forms],
+  darkMode: ["class"],
+
+  plugins: [forms],
 };
