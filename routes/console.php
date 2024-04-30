c9e27430e4b1745976aa1adc68707ad6c55ace8f
Nursandi <sandinur157@gmail.com>
2024-04-30 23:30:28 +0700

    feat: Register queue:listen command in console.php

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:clear-inactive-users')->everyTenSeconds();
+Schedule::command('app:clear-inactive-users')->everyMinute();
+Schedule::command('queue:listen')->everyMinute();
