f2ad16628862f32be1f58938a429ca99420ddcbd
Nursandi <sandinur157@gmail.com>
2024-05-07 07:33:37 +0700

    feat: Set default avatar on registering user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,6 +41,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'avatar' => '/images/avatar.png'
         ]);
 
         event(new Registered($user));
